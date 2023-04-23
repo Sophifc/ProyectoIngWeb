2d556fc6d1780d5baeac228080268f30e76bdbe0
Sophifc <88857873+Sophifc@users.noreply.github.com>
2023-04-23 14:45:38 -0400

    guardar datos

---
+++ JS/validacionRegistro.js
@@ -0,0 +1,69 @@
+const nombre = document.getElementById("name");
+const usuario = document.getElementById("user");
+const email = document.getElementById("email");
+const pass = document.getElementById("contra");
+const pass2 = document.getElementById("contra2");
+const form = document.getElementById("formulario");
+const telefono = document.getElementById("telefono");
+const parrafo = document.getElementById("warnings");
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = "";
+    let entrar = false;
+    let isEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+	let isTelefono = /^\d{7,14}$/;
+	let isUsuario = /^[a-zA-Z0-9\_\-]{4,16}$/;
+	let isPass=  /^.{4,12}$/;
+	let isNombre= /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+    parrafo.innerHTML = "";
+
+    var nombre1 = document.getElementById("name").value;
+    console.log(nombre1);
+
+    var usuario1 = document.getElementById("user").value;
+    console.log(usuario1);
+
+    var email1 = document.getElementById("email").value;
+    console.log(email1);
+
+    var contra1 = document.getElementById("contra").value;
+    console.log(contra1);
+
+    if(!isNombre.test(nombre.value)){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+
+    if(!isEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+
+	if(!isTelefono.test(telefono.value)){
+        warnings += `El telefono no es valido <br>`
+        entrar = true
+    }
+
+	if(!isUsuario.test(usuario.value)){
+        warnings += `El usuario no es valido <br>`
+        entrar = true
+    }
+
+	if(!isPass.test(pass.value)){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+
+	if(pass.value !== pass2.value){
+		warnings += `Las contraseñas no son iguales <br>`
+        entrar = true
+	}
+
+	
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
+})
